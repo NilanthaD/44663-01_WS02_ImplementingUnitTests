376ad3885edda7723e3574406febc20d3fa4d475
Nilantha, Dambadeni Kalu Achchillage <s528116@nwmissouri.edu>
2019-02-06 12:08:02 -0600

    add few failing tests

--- XUnitDemo/XUnitDemoTest/CalculatorTest.cs
+++ XUnitDemo/XUnitDemoTest/CalculatorTest.cs
@@ -21,12 +21,26 @@ namespace XUnitDemoTest
         [Theory]
         [InlineData(5, 3, 2)] // Pass test
         [InlineData(4, 7, -3)] // pass test
-        [InlineData(6, -3, -3)] // fail test
-        public void Add_Test1(double a, double b, double c)
+        [InlineData(-6, -3, -3)] // pass test
+        [InlineData(5.25, 2.75, 2.5)]// pass test
+        [InlineData(Double.MaxValue, Double.MaxValue, 20)] // passs test - Edge test
+        public void Add_PassTestMultipleParameterSets(double a, double b, double c)
         {
             double expected = a;
             double actual = Calculator.Add(b, c);
             Assert.Equal(expected, actual);          
+        }
+
+        [Theory]
+        [InlineData(6,2,3)]
+        [InlineData(-4,3,-6)]
+        
+
+        public void Add_FailTestMultipleParameterSets(double a, double b, double c)
+        {
+            double expected = a;
+            double actual = Calculator.Add(b, c);
+            Assert.Equal(expected, actual);
         }
     }
 }
